33ff1cbb924ca71855293176d946cfa2d45cae14
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-06-10 08:38:23 +0530

    New Resume

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -81,7 +81,7 @@ export default function Portfolio() {
               <button className="btn-contact" onClick={() => scrollTo('Contact')}>
                 Contact
               </button>
-              <a className="btn-resume" href="/Aakarsh-Resume.pdf" download>
+              <a className="btn-resume" href="/Aakarsh Singh - Resume.pdf" download>
                 Resume <span className="btn-arrow">↓</span>
               </a>
             </div>
